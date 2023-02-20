25daabbdd20fffa41a850b077737db8bf9db4c14
Biruk Anley <birukanley2022@gmail.com>
2023-02-20 21:51:54 +0300

    update app style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  let headerTitle = "Aura Program Go for it"
+  const [title, setTitle]=useState(headerTitle)
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title}</h1>
+      <button onClick={() => setTitle("Michigan Go for It")}>
+         Change Title
+      </button>
+
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment Counter
+      </button>
+      <p>{count}</p>
     </div>
   );
 }
