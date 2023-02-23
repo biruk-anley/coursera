811a883c84669d595b87e28c9e415f769047c7fd
Biruk Anley <birukanley2022@gmail.com>
2023-02-23 17:32:42 +0300

    update app file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  let headerTitle = "Aura Program Go for it"
-  const [title, setTitle]=useState(headerTitle)
-  const [count, setCount] = useState(0);
+  
   return (
     <div>
-      <h1>{title}</h1>
-      <button onClick={() => setTitle("Michigan Go for It")}>
-         Change Title
-      </button>
-
-
-      <button onClick={() => setCount(count + 1)}>
-        Increment Counter
-      </button>
-      <p>{count}</p>
+      <h1>
+        Hello World!
+      </h1>
+      <p>h</p>
     </div>
   );
 }
