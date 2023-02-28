95aea876a9d715c50b22efd0f7b1ca73f699ff10
Biruk Anley <birukanley2022@gmail.com>
2023-02-28 16:23:54 +0300

    update changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
-import App from "./App";
+// import './api/server'
+import { Provider } from 'react-redux'
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(
+  <React.StrictMode>
+    
+      <App />
+ 
+  </React.StrictMode>,
+  document.getElementById('root')
+)
